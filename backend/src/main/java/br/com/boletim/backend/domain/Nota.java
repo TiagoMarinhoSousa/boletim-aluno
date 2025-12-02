455422f2ca38ae42176513ac757f40d03f1a9fa2
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2025-12-02 14:24:35 -0300

    feat(domain): cria entidade Nota com getters e setters

---
+++ backend/src/main/java/br/com/boletim/backend/domain/Nota.java
@@ -0,0 +1,54 @@
+package br.com.boletim.backend.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "avaliacao_id")
+    private Avaliacao avaliacao;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Avaliacao getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Avaliacao avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+}
