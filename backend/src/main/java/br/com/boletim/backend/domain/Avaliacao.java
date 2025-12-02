0118708b6bbaf8e5f2370dff2c031c087d6200e7
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2025-12-02 12:55:30 -0300

    feat(domain): cria entidade Avaliacao com getters e setters

---
+++ backend/src/main/java/br/com/boletim/backend/domain/Avaliacao.java
@@ -0,0 +1,54 @@
+package br.com.boletim.backend.domain;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Avaliacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id")
+    private Disciplina disciplina;
+
+    @OneToMany(mappedBy = "avaliacao")
+    private List<Nota> notas;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public List<Nota> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<Nota> notas) {
+        this.notas = notas;
+    }
+}
