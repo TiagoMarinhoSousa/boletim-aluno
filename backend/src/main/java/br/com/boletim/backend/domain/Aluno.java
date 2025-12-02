0de2e64a74e8859b42510af73cadd4ef8ce7110a
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2025-12-02 12:53:04 -0300

    feat(domain): cria entidade Aluno com getters e setters

---
+++ backend/src/main/java/br/com/boletim/backend/domain/Aluno.java
@@ -0,0 +1,54 @@
+package br.com.boletim.backend.domain;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "turma_id")
+    private Turma turma;
+
+    @OneToMany(mappedBy = "aluno")
+    private List<Nota> notas;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public List<Nota> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<Nota> notas) {
+        this.notas = notas;
+    }
+}
